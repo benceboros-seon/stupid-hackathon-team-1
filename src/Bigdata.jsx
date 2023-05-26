0823e20906fc43b015da0bbe847c5afad9e3fab9
benceboros-seon <bence.boros@seon.io>
2023-05-26 17:32:31 +0200

    commit8

---
+++ src/Bigdata.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  .h1 {
+    font-size: 50px;
+  }
+  display: absolute;
+`
+
+export default () => (
+  <Container>
+  <h1>Big Data</h1>
+  </Container>
+)
