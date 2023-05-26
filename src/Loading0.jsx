f0fe3f1b11d59a90d81655c3d2fb15285e9cbd18
benceboros-seon <bence.boros@seon.io>
2023-05-26 15:57:30 +0200

    add build

--- src/Loading0.jsx
+++ src/Loading0.jsx
@@ -1,5 +1,97 @@
+import styled from 'styled-components';
+
 const Loading0 = () => (
-  <div>Loading0</div>
+  <div class="content">
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+  <div class="cuboid">
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+    <div class="side"></div>
+  </div>
+</div>
 )
 
-export default Loading0;
+const Container = styled.div({
+	margin: '0',
+	padding: '0',
+	width: '100vw',
+	height: '100vh',
+	overflow: 'hidden',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	perspective: '100vmin',
+	background: 'radial-gradient(circle at 50% 100%, #212121, #000)',
+})
+
+export default () => (
+  <Container>
+    <Loading0 />
+  </Container>
+);
